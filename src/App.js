111e501dbe8288d60a78e332112273fe2332ed98
ShinHEE-git <86251113+ShinHEE-git@users.noreply.github.com>
2022-03-11 21:24:31 +0900

    delete기능 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import './App.css';
-import { useState } from 'react';
+import { FaPen, FaTrashAlt } from "react-icons/fa";
+import { useState, useRef } from 'react';
 
 export default function App() {
-  const [todoItemList, setTodoItemList] = useState([])
+  const [todoItemList, setTodoItemList] = useState(["eat kimchi", "update Portfolio"])
+  const inputForm = useRef(null)
 
-
-  function createItemList(e) {
+  function createItem(e) {
     e.preventDefault();
     const copyTodoItemList = todoItemList.concat(e.target[0].value)
     setTodoItemList(copyTodoItemList)
     e.target[0].value = ""
+  }
 
+  function updateItem(i) {
+
+  }
+
+  function deleteItem(i) {
+    const copyTodoItemList = [...todoItemList]
+    copyTodoItemList.splice(i, 1)
+    setTodoItemList(copyTodoItemList)
   }
 
   return (
     <>
       <div>
         <ul>
-          {todoItemList.map((item, i) => <li key={i}>{item}</li>)}
+          {todoItemList.map((item, i) => {
+            return (
+              <li key={i}>{item}
+                <button onClick={() => updateItem(i)}><FaPen /></button>
+                <button onClick={() => deleteItem(i)}><FaTrashAlt key={i} /></button>
+              </li>)
+          })}
         </ul>
       </div>
-      <form onSubmit={createItemList}>
+      <form onSubmit={createItem} ref={inputForm}>
         <input type="text" />
         <input type="submit" value="submit" />
       </form>
