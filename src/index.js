be6619c5e52f78950950951dd2af4eeb7eefc222
ShinHEE-git <86251113+ShinHEE-git@users.noreply.github.com>
2022-03-15 21:49:20 +0900

    add CSS

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <header>
       <h1>To Do List (with React)</h1>
     </header>
-    <App />
+    <div className='content'>
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
